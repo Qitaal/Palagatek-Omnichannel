b92c75f60f5b006904ef868c03ac9d79df1a5fb2
Thalib Abu Qitaal <thalib74@gmail.com>
2022-11-13 21:41:09 +0700

    [PAL-31] Implement django auth login logout

--- config/settings/base.py
+++ config/settings/base.py
@@ -147,3 +147,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
